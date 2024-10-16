91d82ba2f06319ce2269122a1ceadbd7cfb8629d
Julia <JuliaYlibka@yandex.com>
2024-10-16 17:50:08 +0300

    First Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,28 +10,27 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        ArrayList<Integer> ints = SetInts();
-        ArrayList<Integer> uniqueNumbers = new ArrayList<>();
-        for (int number : ints) {
-            if (!uniqueNumbers.contains(number)) {
-                uniqueNumbers.add(number);
-            }
+        ArrayList<Integer> ints = new ArrayList<Integer>();
+        ArrayList<Double> doubles = new ArrayList<Double>();
+        for(int i=0;i<10;i++)
+            ints.add(i);
+        for(double j=0.0;j<=2;j+=0.1)
+            doubles.add(j);
+        System.out.println("Максимальное натуральное число: "+ max(ints));
+        System.out.println("Максимальное число: "+ max(doubles));
+
         }
-        System.out.println("Несовпадающие числа в последовательности:");
-        for (int uniqueNumber : uniqueNumbers) {
-            System.out.println(uniqueNumber);
-        }
-    }
-        public static ArrayList<Integer> SetInts() {
-            Scanner in = new Scanner(System.in);
-            ArrayList<Integer> ints = new ArrayList<Integer>();
-            System.out.println("Введите числа. После введения последовательности чисел введите 0.");
-            while (true) {
-                int input = in.nextInt();
-                if (input == 0)
-                    break;
-                ints.add(input);
+        public static <E extends Comparable<E>> E max(ArrayList<E> list) {
+            E res=list.get(0);
+            if (list.isEmpty())
+                throw new IllegalArgumentException("Дан пустой лист!");
+            else {
+                for (int i = 0; i < list.size() ; i++) {
+                    if(list.get(i).compareTo(res)>0){
+                        res = list.get(i);
+                    }
+                }
+                return res;
             }
-            return ints;
         }
     }
